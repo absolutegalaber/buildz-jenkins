07e8ba7d0cc8f17ac0d4240ca1338cc2959d4149
josip <josip.mihelko@gmail.com>
2020-11-20 12:57:08 +0100

    Toying Around.

--- src/com/absolutegalaber/buildz/jenkins/MyBuild.groovy
+++ src/com/absolutegalaber/buildz/jenkins/MyBuild.groovy
@@ -5,9 +5,16 @@ import groovy.json.JsonSlurper
 class MyBuild {
 
     static Long buildNumber(script, String project, String branch) {
+        String nextVersionRequest = """
+{
+    "project": "${project}",
+    "branch":"${branch}"
+}
+"""
+        script.writeFile file: 'nextVersionRequest.json', text: nextVersionRequest
         String response = script.sh(
                 returnStdout: true,
-                script: """curl -X POST -H "Content-Type: application/json" --data "{"project": "${project}","branch":"${branch}"}" http://buildz-api-vpint05.vpint.o2online.de/api/v1/build-numbers/next"""
+                script: """curl -X POST -H "Content-Type: application/json" --data @nextVersionRequest.json http://buildz-api-vpint05.vpint.o2online.de/api/v1/build-numbers/next"""
         )
         //read the returned json into a string
         return new JsonSlurper().parseText(response).counter
