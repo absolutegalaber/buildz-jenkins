21956b2c0ebd4352fc165e7ff97429d55f534186
josip <josip.mihelko@gmail.com>
2020-11-20 14:57:53 +0100

    Toying Around.

--- src/com/absolutegalaber/buildz/jenkins/MyBuild.groovy
+++ src/com/absolutegalaber/buildz/jenkins/MyBuild.groovy
@@ -15,7 +15,7 @@ class MyBuild implements Serializable {
         script.writeFile file: 'nextVersionRequest.json', text: nextVersionRequest
         String response = script.sh(
                 returnStdout: true,
-                script: """curl -X POST -H "Content-Type: application/json" --data @nextVersionRequest.json http://buildz-api-vpint05.vpint.o2online.de/api/v1/build-numbers/next"""
+                script: """curl -X POST -H "Content-Type: application/json" --data @nextVersionRequest.json ${script.env.BUILDZ_API}/api/v1/build-numbers/next"""
         )
         //read the returned json into a string
         return new JsonSlurper().parseText(response).counter
@@ -32,7 +32,7 @@ class MyBuild implements Serializable {
         script.writeFile file: 'createBuildRequest.json', text: createBuildRequest
         String response = script.sh(
                 returnStdout: true,
-                script: """curl -X POST -H "Content-Type: application/json" --data @createBuildRequest.json http://buildz-api-vpint05.vpint.o2online.de/api/v1/builds/create"""
+                script: """curl -X POST -H "Content-Type: application/json" --data @createBuildRequest.json ${script.env.BUILDZ_API}/api/v1/builds/create"""
         )
         //read the returned json into a string
         return new JsonSlurper().parseText(response).id
@@ -41,7 +41,7 @@ class MyBuild implements Serializable {
     static void addBuildzLabel(script, Long buildId, String key, String value) {
         script.writeFile file: 'addLabelsRequest.json', text: """[{"key":"${key}", "value": "${value}"}]"""
         script.sh(
-                script: """curl -X POST -H "Content-Type: application/json" --data @addLabelsRequest.json http://buildz-api-vpint05.vpint.o2online.de/api/v1/builds/add-labels/${buildId}"""
+                script: """curl -X POST -H "Content-Type: application/json" --data @addLabelsRequest.json ${script.env.BUILDZ_API}/api/v1/builds/add-labels/${buildId}"""
         )
     }
 }
