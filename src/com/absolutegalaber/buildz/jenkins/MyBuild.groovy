0add9ef85cc771395fcea4425a2d3f98da70def2
josip <josip.mihelko@gmail.com>
2020-11-20 13:34:21 +0100

    Toying Around.

--- src/com/absolutegalaber/buildz/jenkins/MyBuild.groovy
+++ src/com/absolutegalaber/buildz/jenkins/MyBuild.groovy
@@ -1,5 +1,6 @@
 package com.absolutegalaber.buildz.jenkins
 
+import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 
 class MyBuild {
@@ -18,5 +19,31 @@ class MyBuild {
         )
         //read the returned json into a string
         return new JsonSlurper().parseText(response).counter
+    }
+
+    static Long createBuild(script, String project, String branch, Long buildNumber) {
+        String createBuildRequest = """
+{
+    "project": "${project}",
+    "branch":"${branch}",
+    "buildNumber":${buildNumber}
+}
+"""
+        script.writeFile file: 'createBuildRequest.json', text: createBuildRequest
+        String response = script.sh(
+                returnStdout: true,
+                script: """curl -X POST -H "Content-Type: application/json" --data @createBuildRequest.json http://buildz-api-vpint05.vpint.o2online.de/api/v1/builds/create"""
+        )
+        //read the returned json into a string
+        return new JsonSlurper().parseText(response).id
+    }
+
+    static void addLabels(script, Long buildId, Map<String, String> labels) {
+
+        String addLabelsRequest = new JsonOutput().toJson(labels)
+        script.writeFile file: 'addLabelsRequest.json', text: addLabelsRequest
+        script.sh(
+                script: """curl -X POST -H "Content-Type: application/json" --data @addLabelsRequest.json http://buildz-api-vpint05.vpint.o2online.de/api/v1/builds/add-labels/${buildId}"""
+        )
     }
 }
